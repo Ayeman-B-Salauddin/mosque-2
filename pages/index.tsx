b6957c3ff804690a3d0a19c47057649c777afa18
ayeman <ayemanbsalauddin@gmail.com>
2022-11-16 18:57:51 +0600

    d

--- pages/index.tsx
+++ pages/index.tsx
@@ -28,7 +28,7 @@ export default function Home() {
         <Hero />
         {/* <MessengerApp /> */}
         <MessengerChat
-          pageId="1862102394024693"
+          pageId="191808807846781"
           language="sv_SE"
           themeColor={"#000000"}
           bottomSpacing={300}
@@ -63,3 +63,34 @@ export default function Home() {
     </div>
   );
 }
+
+// <!-- Messenger Chat plugin Code -->
+//     <div id="fb-root"></div>
+
+//     <!-- Your Chat plugin code -->
+//     <div id="fb-customer-chat" class="fb-customerchat">
+//     </div>
+
+//     <script>
+//       var chatbox = document.getElementById('fb-customer-chat');
+//       chatbox.setAttribute("page_id", "191808807846781");
+//       chatbox.setAttribute("attribution", "biz_inbox");
+//     </script>
+
+//     <!-- Your SDK code -->
+//     <script>
+//       window.fbAsyncInit = function() {
+//         FB.init({
+//           xfbml            : true,
+//           version          : 'v15.0'
+//         });
+//       };
+
+//       (function(d, s, id) {
+//         var js, fjs = d.getElementsByTagName(s)[0];
+//         if (d.getElementById(id)) return;
+//         js = d.createElement(s); js.id = id;
+//         js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+//         fjs.parentNode.insertBefore(js, fjs);
+//       }(document, 'script', 'facebook-jssdk'));
+//     </script>
